e28e60d0aeb683a9a3fb9215eb776a36a15448ec
Ivan Sobotovic <ivan.sobotovic@dyves.com>
2026-02-25 15:56:32 +0100

    SEO optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://www.toolaroo.net',
   output: 'static',
   integrations: [
     tailwind(),
-    react()
+    react(),
+    sitemap({
+      filter: (page) => !page.includes('/admin'),
+      changefreq: 'weekly',
+      priority: 0.7,
+    })
   ],
 });
